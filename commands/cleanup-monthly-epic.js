4ee7304684e4620d2d1ff791579eeadb1a8a2205
Makoto OOSUGI <oosugi20@gmail.com>
2019-10-28 16:01:14 +0900

    引数化: cleanupの方

--- commands/cleanup-monthly-epic.js
+++ commands/cleanup-monthly-epic.js
@@ -1,9 +1,13 @@
 const runner = require('../main.js');
 
-exports.command = 'cleanup-monthly-epic <issueNumber>';
+exports.command = 'cleanup-monthly-epic <ownerName> <repoName> <issueNumber>';
 exports.desc = '月時まとめEpicを整理する';
 exports.builder = {};
 exports.handler = async function(argv) {
-  const res = await runner.cleanupMonthlyEpic(argv.issueNumber);
+  const res = await runner.cleanupMonthlyEpic(
+    argv.ownerName,
+    argv.repoName,
+    argv.issueNumber
+  );
   //console.log(res.data.issues);
 };
