74cd78b02f2be34bed373ea96740f185c935e067
Makoto OOSUGI <oosugi20@gmail.com>
2019-10-28 15:48:27 +0900

    不要ファイル削除

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
 console.log('zenhub-runner');
 
-const fetchIssues = require('./lib/fetch-issues.js');
-const filterNoEstimate = require('./lib/filter-no-estimate.js');
-
-
 const Issues = require('./lib/Issues');
 
 module.exports = {
@@ -12,15 +8,6 @@ module.exports = {
     await myIssues.fetch();
 
     return myIssues.filterClosedAndNoEstimate();
-
-    //const filterIssues = await _filterIssues();
-    //return filterIssues;
-
-    //async function _filterIssues() {
-    //  const issues = await fetchIssues();
-    //  const filterdIssues = filterNoEstimate(issues);
-    //  return filterdIssues;
-    //}
   },
 
   cleanupMonthlyEpic: require('./lib/cleanup-monthly-epic.js')
