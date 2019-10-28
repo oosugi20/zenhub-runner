166cc5cf3e7deb2d03b5d67df93282260910258a
Makoto OOSUGI <oosugi20@gmail.com>
2019-10-28 17:17:45 +0900

    key名を定数に

--- commands/check-no-estimate-issues.js
+++ commands/check-no-estimate-issues.js
@@ -4,6 +4,9 @@ const opener = require('opener');
 
 const runner = require('../main.js');
 
+const UPDATE_KEY = '[UPDATE]';
+const QUIT_KEY = '[QUIT]';
+
 exports.command = 'check-no-estimate-issues <ownerName> <repoName>';
 exports.desc = 'estimateが設定されていないIssueがないか確認する';
 exports.builder = {};
@@ -26,16 +29,16 @@ exports.handler = async function(argv) {
         type: 'list',
         name: 'selectedUrl',
         message: 'select url',
-        choices: _.concat(urls, new inquirer.Separator(), 'UPDATE', 'QUIT'),
+        choices: _.concat(urls, new inquirer.Separator(), UPDATE_KEY, QUIT_KEY),
       }
     ];
     const answers = await inquirer.prompt(questions);
 
-    if (answers.selectedUrl === 'QUIT') {
+    if (answers.selectedUrl === QUIT_KEY) {
       return;
     }
 
-    if (answers.selectedUrl === 'UPDATE') {
+    if (answers.selectedUrl === UPDATE_KEY) {
       const newUrls = await fetchUrls();
       await itr(newUrls);
       return;
