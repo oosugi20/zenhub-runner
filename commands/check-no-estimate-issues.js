d58e9def1c242bc5755751141ae2e0a2a77c0a9f
Makoto OOSUGI <oosugi20@gmail.com>
2019-10-28 17:01:10 +0900

    promptで開けるように

--- commands/check-no-estimate-issues.js
+++ commands/check-no-estimate-issues.js
@@ -1,4 +1,7 @@
 const _ = require('lodash');
+const inquirer = require('inquirer');
+const opener = require('opener');
+
 const runner = require('../main.js');
 
 exports.command = 'check-no-estimate-issues <ownerName> <repoName>';
@@ -8,5 +11,28 @@ exports.handler = async function(argv) {
   const filteredIssues = await runner.noEstimateIssues(argv.ownerName, argv.repoName);
 	//console.log('aa %s bb', argv.repoName);
   const urls = _.map(filteredIssues, (issue) => issue.getHtmlUrl());
-  console.log(urls);
+
+  const questions = [
+    {
+      type: 'list',
+      name: 'selectedUrl',
+      message: 'select url',
+      choices: _.concat(urls, new inquirer.Separator(), 'QUIT'),
+    }
+  ];
+
+  itr();
+
+  //let quitFlag = false;
+
+  function itr() {
+    inquirer.prompt(questions).then(answers => {
+      if (answers.selectedUrl === 'QUIT') {
+        //quitFlag = true;
+        return;
+      }
+      opener(answers.selectedUrl);
+      itr();
+    });
+  }
 };
